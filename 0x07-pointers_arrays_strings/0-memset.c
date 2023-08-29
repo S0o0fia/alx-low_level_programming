8f816167de0d9ad06e5f51b3b44718f42ea23acf
S0o0fia <kamelsafaa960@gmail.com>
2023-08-29 01:16:40 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,12 +9,11 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned int i = 0;
 
-	for (i = 0; i < n; i++)
+	for (; i < n; i++)
 	{
-		*(s + i) = b;
-		
+		*(s + i) = b;		
 	}
 	return (s);
 }
