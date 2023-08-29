d7631d7d81645a29621606d2ed074891ea744342
S0o0fia <kamelsafaa960@gmail.com>
2023-08-29 01:10:03 -0700

    add task 0 and headers

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - fill a block of memory
+ * @s: starting address of memory
+ * @b: the desired value
+ * @n: member of bytes to be changed
+ *
+ * Return: Always value
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i=0; i<n; i++)
+	{
+		*(s + i) = b;
+		
+	}
+	return (s);
+}
